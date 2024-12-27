cb68c34873b3b9f5005dd79684864b98794b48ce
LucianoNascimento <dev5@ezsoft.com.br>
2024-12-27 20:15:53 -0300

    refatorando oportunidades

--- fatec_conectada/app/Http/Controllers/OportunidadeController.php
+++ fatec_conectada/app/Http/Controllers/OportunidadeController.php
@@ -2,20 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OportunidadeRequest;
 use App\Models\Oportunidade;
 
 class OportunidadeController extends Controller
 {
 
-    private  $oportunidade;
+    private $oportunidade;
 
     public function __construct(Oportunidade $oportunidade)
     {
         $this->oportunidade = $oportunidade;
     }
+
     public function index()
     {
         return $this->oportunidade->listarOportunidade();
+    }
+
+    public function store(OportunidadeRequest $request)
+    {
+        $validarOportunidade = $request->validated();
+        $oportunidade = $this->oportunidade->cadastrarOportunidade($validarOportunidade);
+        return response()->json([
+            'message' => 'Oportunidade cadastrada com sucesso',
+            'oportunidade' => $oportunidade,
+        ], 201);
+    }
+
+    public function update(OportunidadeRequest $request, int $id)
+    {
+        $validarOportunidade = $request->validated();
+        $oportunidade = $this->oportunidade->editarOportunidade($validarOportunidade, $id);
+        return response()->json([
+            'message' => 'Oportunidade editada com sucesso',
+            'oportunidade' => $oportunidade,
+        ], 200);
     }
 
     /**
@@ -37,6 +59,15 @@ class OportunidadeController extends Controller
             'sucess' => true,
             'data' => $oportunidade
         ], 200);
+    }
+
+    public function destroy(int $id)
+    {
+        $this->oportunidade->excluirOportunidade($id);
+        return response()->json([
+            'message' => 'Oportunidade excluída com sucesso',
+        ]);
+
     }
 
 }
